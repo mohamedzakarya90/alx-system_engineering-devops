616ee8fe78958445f7ebe3d1bdee96a68059acda
mohamed zakarya mohamed farag <mohamed.zakrya.farag@gmail.com>
2024-05-17 08:12:42 +0000

    0x1B-web_stack_debugging_4

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# Enabling user holberton to login and opening files without error
+
+# Increasing the hard file limit for Holberton User
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increasing soft file limit for Holberton User
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
